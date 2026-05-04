0dd830018b347c0e0b7e3553f83df0d19c992fdc
mhunt <57237495+mhuntno@users.noreply.github.com>
2026-05-04 12:41:53 +0200

    25 add args for port and local operation (#26)
    * add args for port and local operation
    
    * updated default port

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import argparse
 from concurrent import futures
 from pathlib import Path
 
@@ -12,12 +13,33 @@ def serve():
     """
     Create and start gRPC server and serve it.
     """
+    parser = argparse.ArgumentParser(
+        prog="skavl-anomaly-detection-module",
+        description="Anomaly detection in aerial images")
+
+    parser.add_argument("-p", "--port", help="Port to start tiler server with", default=50053)
+    parser.add_argument("-l", "--local", action="store_true",
+                        help="""Determines if all or only local connections should be accepted. 
+                                   If this argument is present, the servers IP will be 127.0.0.1, 
+                                   if this argument is omitted, the ip will be set to 0.0.0.0 meaning accept all connections""")
+
+    args = parser.parse_args()
+
     Path("reports").mkdir(exist_ok=True)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     rpg2.add_ReportServiceServicer_to_server(ReportServicer(), server)
-    server.add_insecure_port("0.0.0.0:50053")
+
+    # Accepts connections only locally when running locally.
+    server_port = getattr(args, "port")
+    server_ip = ""
+    if getattr(args, "local", False):
+        server_ip = "127.0.0.1"
+    else:
+        server_ip = "0.0.0.0"
+    server.add_insecure_port(f"{server_ip}:{server_port}")
+
     server.start()
-    print("gRPC server listening on 0.0.0.0:50053")
+    print(f"gRPC server listening on {server_ip}:{server_port}")
     try:
         while True:
             time.sleep(3600)
